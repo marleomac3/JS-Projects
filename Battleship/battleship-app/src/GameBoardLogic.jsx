405f39b5ef6691aecf1702fc3199b68e81a238ae
Marion McWilliams <marionmcwilliams@Marions-MacBook-Air.local>
2024-12-30 11:37:43 -0500

    feat: Added a hits tracker and a cell ID function

--- Battleship/battleship-app/src/GameBoardLogic.jsx
+++ Battleship/battleship-app/src/GameBoardLogic.jsx
@@ -13,14 +13,23 @@ export default class GameBoardLogic {
         this.player1ShipsSunk = 0;
         this.player2ShipsSunk = 0;
         this.ships = {
-            carrier: {length: 5, isSunk: false, coords: [], orientation: "horizontal"},
-            battleship: {length: 4, isSunk: false, coords: [], orientation: "horizontal"},
-            cruiser: {length: 3, isSunk: false, coords: [], orientation: "horizontal"},
-            submarine: {length: 3, isSunk: false, coords: [], orientation: "horizontal"},
-            destroyer: {length: 2, isSunk: false, coords: [], orientation: "horizontal"}
+            carrier: {length: 5, numberOfHits: 0, isSunk: false, coords: [], orientation: "horizontal"},
+            battleship: {length: 4, numberOfHits: 0, isSunk: false, coords: [], orientation: "horizontal"},
+            cruiser: {length: 3, numberOfHits: 0, isSunk: false, coords: [], orientation: "horizontal"},
+            submarine: {length: 3, numberOfHits: 0, isSunk: false, coords: [], orientation: "horizontal"},
+            destroyer: {length: 2, numberOfHits: 0, isSunk: false, coords: [], orientation: "horizontal"}
         };
+
+        this.assignCellIds();
     }
 
-    
+    assignCellIds() {
+        document.querySelectorAll(".row").forEach((row) => {
+            const rowId = row.id.split("-")[1];
+            row.querySelectorAll(".cell").forEach((cell, index) => {
+                cell.id = `${rowId}${index + 1}`;
+            });
+        });
+    }
 
 }
